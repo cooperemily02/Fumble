498e3c252061074a534f915dcdadc44a36803ac5
Shambhavi Ramaswamy <55058421+shambhavir@users.noreply.github.com>
2021-01-07 18:31:37 -0500

    built npm

---
+++ build/precache-manifest.f1afe6e8de343e666a97812b9944b140.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a6c82c15c0f24e3792480ece9ccd30",
+    "url": "/react-tinder-card-demo/index.html"
+  },
+  {
+    "revision": "6dda23436ca6c2eb06f1",
+    "url": "/react-tinder-card-demo/static/css/main.39a3c346.chunk.css"
+  },
+  {
+    "revision": "c035cdb08adea761f478",
+    "url": "/react-tinder-card-demo/static/js/2.8371661a.chunk.js"
+  },
+  {
+    "revision": "6dda23436ca6c2eb06f1",
+    "url": "/react-tinder-card-demo/static/js/main.a7d3b9e0.chunk.js"
+  },
+  {
+    "revision": "25361a0eda5e176322ee",
+    "url": "/react-tinder-card-demo/static/js/runtime-main.973bd8d6.js"
+  },
+  {
+    "revision": "4833a159996e2d0e982dfa3d6fbf7c0a",
+    "url": "/react-tinder-card-demo/static/media/background.4833a159.jpg"
+  }
+]);
