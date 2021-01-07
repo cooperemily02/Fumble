d64131b600e60eda76206a606bdb82c6680d2cf9
Shambhavi Ramaswamy <55058421+shambhavir@users.noreply.github.com>
2021-01-07 12:58:29 -0500

    Added modal, combined all changes

--- src/App.js
+++ src/App.js
@@ -5,14 +5,23 @@ import Switch from 'react-ios-switch'
 import Advanced from './examples/Advanced'
 import Simple from './examples/Simple'
 
+
+
 function App () {
+  const [arrayofplaces, setplaces]  =  useState(true)
   const [showAdvanced, setShowAdvanced] = useState(true)
 
   return (
     <div className='app'>
-      {showAdvanced ? <Simple /> : <Simple />}
+        <Simple/>
       <div className='row'>
         {/* <p style={{ color: '#fff' }}>Show advanced example</p> <Switch checked={showAdvanced} onChange={setShowAdvanced} /> */}
+            <div id="myModal" class="modal">
+                <div class="modal-content">
+                  <span class="close">&times;</span>
+                  <p>NEED TO ADD THE DEAL HERE</p>
+                </div>
+            </div>
       </div>
     </div>
   )
--- src/examples/Simple.js
+++ src/examples/Simple.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 // import TinderCard from '../react-tinder-card/index'
 import TinderCard from 'react-tinder-card'
-
+import ReactDOM from 'react-dom';
+const MongoClient = require('mongodb').MongoClient
+//MongoClient.connect('mongodb+srv://shambhavir:Success2021!!@cluster0.xrzxq.mongodb.net/fumble?retryWrites=true&w=majority'
 const db = [
   {
     name: 'Taco Bell',
@@ -27,28 +29,99 @@ const db = [
     name: 'Wawa',
     url: './img/dinesh.jpg',
     price: 5
+  },
+  {
+    name: 'comfort food',
+    type: 'category'
   }
 ]
 
-var swipedRight = []
-var swipes = 0
+// function callAPI () {
+//   if (true) {
+//     fetch('https://api.geoapify.com/v2/places?categories=commercial.food_and_drink&filter=circle:-72.519867,42.375801,5000&limit=20&apiKey=1af6a8fa67c94cba9c73377e2866a995').then(function (response) {
+//       return response.json();
+//     }).then(function (result) {
+//       for(var i = 0; i<result.features.length; i++){
+//         arrayofplaces.push({name: result.features[i].properties.name, url: './img/richard.jpg', price: 1})
+//       }
+//     });
+//   }
+// }
+
+// var swipedRight = []
+// var currentChoices = [0, 0, 0, 0] 
+// var swipes = 0
 function Simple () {
-  const characters = db
-
+  var temparray = []
+  const [arrayofplaces, setarray] = useState([{name: 'No Name', url: './img/richard.jpg', price: 1}])
+  useEffect(() => {
+    fetch('https://api.discountapi.com/v2/deals?category_slugs=restaurants&location=longmeadow&api_key=KiyYblAt').then(function (response) {
+      return response.json();
+    }).then(function (result) {
+      console.log(result)
+      for(var i = 0; i<result.deals.length; i++){
+        temparray.push({name: result.deals[i].deal.short_title, url: './img/richard.jpg', price: 1})
+      }
+      console.log(temparray)
+      setarray(temparray)
+    });
+  });
+  
+    
+  // const arrayofplaces = useState([])
+  // callAPI(() =>{
+  //   fetch('https://api.geoapify.com/v2/places?categories=commercial.food_and_drink&filter=circle:-72.519867,42.375801,5000&limit=20&apiKey=1af6a8fa67c94cba9c73377e2866a995').then(function (response) {
+  //     return response.json();
+  //   }).then(function (result) {
+  //     for(var i = 0; i<result.features.length; i++){
+  //       arrayofplaces.push({name: result.features[i].properties.name, url: './img/richard.jpg', price: 1})
+  //     }
+  //   });
+  // }, []);
+  // console.log(arrayofplaces)
+ // const characters = db
   const [lastDirection, setLastDirection] = useState()
+  // var characters = []
+  // characters = arrayofplaces
 
-
-  const swiped = (direction, nameToDelete) => {
+  var swipedRight = []
+  var weightVals = []
+  var swipes = 0
+  const swiped = (direction, nameToDelete, weight) => {
       if(direction === 'right'){
         swipes++
         swipedRight.push(nameToDelete)
-        var length = swipedRight.length
-        console.log(swipedRight)
-        console.log('Test')
+        weightVals.push(weight)
+        // var length = swipedRight.length
+        // console.log(swipedRight)
+        // console.log('Test')
         // give users a minimum number of swipes before giving them the best deal
-        if(swipes === 2) {
+        // var i = 0
+        // while(i < 4)
+        // {
+        //   swipes++
+        //   swipedRight.push(nameToDelete)
+        //   var length = swipedRight.length
+        //   //console.log(swipedRight)
+        //   //console.log('Test')
+        //   currentChoices[i] = db.price 
+        //   i++; 
+        // }
+        
+        // for(i = 0; i < currentChoices.length; i++)
+        //   console.log(currentChoices[i]); 
+        if(swipes === 4) {
           console.log('done')
+          var modal = document.getElementById("myModal");
+          // Get the <span> element that closes the modal
+          var span = document.getElementsByClassName("close")[0];
+          modal.style.display = "block";
+          // When the user clicks on <span> (x), close the modal
+          span.onclick = function() {
+              modal.style.display = "none";
+          }
         }
+
       }
       // const toBeRemoved = cardsLeft[cardsLeft.length - 1].name // Find the card object to be removed
       // const index = db.map(person => person.name).indexOf(toBeRemoved) // Find the index of which to make the reference to
@@ -70,17 +143,37 @@ function Simple () {
       
       <h1>Swipe right on your fave deals!</h1>
       <div className='cardContainer'>
-        {characters.map((character) =>
-          <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
-            <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
-              <h2>{character.name} {character.price}</h2>
+        {arrayofplaces.map((place) =>
+          <TinderCard className='swipe' key={place.name} onSwipe={(dir) => swiped(dir, place.name, place.price)} onCardLeftScreen={() => outOfFrame(place.name)}>
+            <div style={{ backgroundImage: 'url(' + place.url + ')' }} className='card'>
+              <h2>{place.name} {place.price}</h2>
             </div>
           </TinderCard>
         )}
       </div>
       {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
     </div>
-  ) 
+  )
+  
+  // return (
+  //   <div className='readable'>
+  //     <link href='https://fonts.googleapis.com/css?family=Damion&display=swap' rel='stylesheet' />
+  //     <link href='https://fonts.googleapis.com/css?family=Alatsi&display=swap' rel='stylesheet' />
+  //     <link href='https://fonts.googleapis.com/css2?family=Staatliches&display=swap' rel='stylesheet' />
+      
+  //     <h1>Swipe right on your fave deals!</h1>
+  //     <div className='cardContainer'>
+  //       {characters.map((charact) =>
+  //         <TinderCard className='swipe' key={place.name} onSwipe={(dir) => swiped(dir, place.name)} onCardLeftScreen={() => outOfFrame(place.name)}>
+  //           <div style={{ backgroundImage: 'url(' + place.url + ')' }} className='card'>
+  //             <h2>{place.name} {place.sprice}</h2>
+  //           </div>
+  //         </TinderCard>
+  //       )}
+  //     </div>
+  //     {lastDirection ? <h2 className='infoText'>You swiped {lastDirection}</h2> : <h2 className='infoText' />}
+  //   </div>
+  // ) 
 }
 
 export default Simple
