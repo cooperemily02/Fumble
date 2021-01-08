ffe7583ad34fb947a75c70130dca12d81c769ace
cooperemily02 <ecooper@umass.edu>
2021-01-07 20:26:53 -0500

    add saved deals
    wooooo

---
+++ build/precache-manifest.e54dbc7306c9637963ef2f60d3c72c0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b488d9cf62985608c533317b7730186",
+    "url": "/react-tinder-card-demo/index.html"
+  },
+  {
+    "revision": "9ce5c1cd15b395836e58",
+    "url": "/react-tinder-card-demo/static/css/main.6c83b28f.chunk.css"
+  },
+  {
+    "revision": "f67025889ab951e96c64",
+    "url": "/react-tinder-card-demo/static/js/2.f9a2bab6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-tinder-card-demo/static/js/2.f9a2bab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ce5c1cd15b395836e58",
+    "url": "/react-tinder-card-demo/static/js/main.8298c3e8.chunk.js"
+  },
+  {
+    "revision": "858f4fd3cd70409c6d26",
+    "url": "/react-tinder-card-demo/static/js/runtime-main.d27dd23b.js"
+  },
+  {
+    "revision": "4833a159996e2d0e982dfa3d6fbf7c0a",
+    "url": "/react-tinder-card-demo/static/media/background.4833a159.jpg"
+  }
+]);
